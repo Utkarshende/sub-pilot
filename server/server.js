34fb74d51a035b89e682d85623ac8ad74b77b871
Utkarshende <139004110+Utkarshende@users.noreply.github.com>
2026-01-21 22:54:26 +0530

    feat(server): finalize API by mounting subscription routes

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import connectDB from './config/db.js';
 import userLink from './routes/userRoutes.js';
+import subLink from './routes/subRoutes.js'; // Import the new routes
 
 dotenv.config(); // Load .env file
 connectDB();    // Connect to MongoDB
@@ -13,8 +14,11 @@ const app = express();
 app.use(cors()); // Logic: Allows cross-origin requests from Frontend
 app.use(express.json()); // Logic: Allows server to read JSON bodies
 
+
 // ROUTES
 app.use('/api/users', userLink);
+// ROUTES
+app.use('/api/subscriptions', subLink);
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
