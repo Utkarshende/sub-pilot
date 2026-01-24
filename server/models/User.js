6d92ad3c868ade0a296cb8a86e54300a0170bece
Utkarshende <139004110+Utkarshende@users.noreply.github.com>
2026-01-24 23:05:08 +0530

    fix:timestamp added in User.js

--- server/models/User.js
+++ server/models/User.js
@@ -6,9 +6,8 @@ const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   photo: { type: String, default: "" },
-}, { timestamps: true }); // Logic: Automatically adds 'createdAt' and 'updatedAt'
+}, { timestamps: true }); 
 
-// LOGIC: Middleware to hash password before saving
 userSchema.pre('save', async function(next) {
   if (!this.isModified('password')) return next();
   const salt = await bcrypt.genSalt(10);
