004f0c1895c5ae0de65b77cf70bad95f54c0058b
Utkarshende <139004110+Utkarshende@users.noreply.github.com>
2026-01-25 22:41:02 +0530

    fix:get route set up in subroutes.js

--- server/routes/subRoutes.js
+++ server/routes/subRoutes.js
@@ -8,7 +8,6 @@ import jwtCheck from "../middleware/jwtCheck.js";
 
 const router = express.Router();
 
-// LOGIC: Apply middleware to all routes in this file
 router.use(jwtCheck); 
 
 router.get("/", getSubscriptions);
